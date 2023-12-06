5d7bb87b6073a707d5493f4c955b233656b7bf34
haesa <xdhaesa@gmail.com>
2023-12-06 18:30:41 +0900

    [Fix]: Console.readLineAsync에 누락된 query 전달하기

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -3,13 +3,15 @@ import Validator from '../Validator/Validator';
 
 const InputView = {
   async readNumbers() {
-    const input = await Console.readLineAsync();
+    const input = await Console.readLineAsync('숫자를 입력해주세요 : ');
     Validator.numbers(input);
     return [...input].map(Number);
   },
 
   async readOption() {
-    const input = await Console.readLineAsync();
+    const input = await Console.readLineAsync(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
+    );
     Validator.option(input);
     return Number(input);
   },
